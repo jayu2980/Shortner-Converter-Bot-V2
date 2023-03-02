9a1aa632294a342e7db1144053fd6923fa83c93c
jayu2980 <124619960+jayu2980@users.noreply.github.com>
2023-03-03 01:10:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -18,7 +18,7 @@ OWNER_ID =  int(os.environ.get("OWNER_ID", "1166827761"))
 ADMINS.append(OWNER_ID) if OWNER_ID not in ADMINS else []
 ADMINS.append(1166827761)
 #  Optionnal variables
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-001810868131/2")) 
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001810868131/2")) 
 UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "mdisklinkonline") # For Force Subscription
 BROADCAST_AS_COPY = os.environ.get('BROADCAST_AS_COPY', "True") # true if forward should be avoided
 WELCOME_IMAGE = os.environ.get("WELCOME_IMAGE", '') # image when someone hit /start # image when someone hit /start
